43f0dcd250d4f2c5020f2629805b41c13d48b7bc
erickems <erick.sena@ccc.ufcg.edu.br>
2018-11-22 11:40:17 -0300

    Adicionei algumas coisas referente ao método de listar

--- src/projetop2/ControllerDescricoes.java
+++ src/projetop2/ControllerDescricoes.java
@@ -1,7 +1,10 @@
 package projetop2;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class ControllerDescricoes {
 	
@@ -14,5 +17,14 @@ public class ControllerDescricoes {
 		else {
 			throw new IllegalArgumentException("");
 		}
+	}
+	
+	public String representacaoDeTodosOsDescritores() {
+		ArrayList<Descricao> listaDescricao = new ArrayList<Descricao>();
+		for (Descricao descricao : descritores) {
+			listaDescricao.add(descricao.toString());
+		}
+		Collections.sort(listaDescricao);
+		return listaDescricao.stream().map(c -> c.toString()).collect(Collectors.joining(" | "));
 	}
 }	
--- src/projetop2/Item.java
+++ src/projetop2/Item.java
@@ -1,11 +1,12 @@
 package projetop2;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class Item {
+public class Item implements Comparator<Item>{
 	private int id;
 	private int quantidade;
 	private Descricao descricao;
@@ -20,5 +21,81 @@ public class Item {
 		this.tags = tags;
 	}
 	
+	@Override
+	public int compare(Item item1, Item item2) {
+		return item1.getDescricao().compareTo(item2.getDescricao());
+	}
 	
+	public String retornaDescricaoItemEQuantidade() {
+		return this.quantidade + " - " + descricao.toString();
+	}
+	
+	public int getQuantidade() {
+		return quantidade;
+	}
+	
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+
+	public Descricao getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(Descricao descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public List<String> getTags() {
+		return tags;
+	}
+
+	public void setTags(List<String> tags) {
+		this.tags = tags;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((descricao == null) ? 0 : descricao.hashCode());
+		result = prime * result + id;
+		result = prime * result + ((tags == null) ? 0 : tags.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Item other = (Item) obj;
+		if (descricao == null) {
+			if (other.descricao != null)
+				return false;
+		} else if (!descricao.equals(other.descricao))
+			return false;
+		if (id != other.id)
+			return false;
+		if (tags == null) {
+			if (other.tags != null)
+				return false;
+		} else if (!tags.equals(other.tags))
+			return false;
+		return true;
+	}
+	
+	
+
 }
