be15f6bac7f1d791acb60de68f8f208321a60398
deboraleda <debora.souza@ee.ufcg.edu.br>
2018-12-12 22:43:54 -0300

    criada pasta para testes de unidade

---
+++ testes_unidade/entidades/ItemTest.java
@@ -0,0 +1,39 @@
+package entidades;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ItemTest {
+
+	@Test
+	void testItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAtualizaItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRetornaDescricaoEQuantidade() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+}
