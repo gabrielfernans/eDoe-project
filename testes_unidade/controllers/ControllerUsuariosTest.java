fb28b786cf399fe82e77ab9dfe85209ce1ba00e5
gabrielfernans <gabrielwebr@gmail.com>
2018-12-13 18:06:02 -0300

    Métodos parcialmente testados 3

--- testes_unidade/controllers/ControllerUsuariosTest.java
+++ testes_unidade/controllers/ControllerUsuariosTest.java
@@ -19,9 +19,15 @@ class ControllerUsuariosTest {
 		controllerUsuarios.adicionaDoador("1002", "Satya Vaswani", "satya@vishkarcorp.com", "(83) 99221-2571", "PESSOA_FISICA");
 		controllerUsuarios.adicionaDoador("70594610435", "Gabriel Fernandes", "gabrielwebr@gmail.com", "(83) 98606-6330", "PESSOA_FISICA");
 		controllerUsuarios.adicionaDoador("1234", "Manel", "manel@gmail.com", "(83) 98000-0000", "PESSOA_FISICA");
+		controllerUsuarios.adicionaDoador("9090", "Fulano", "fulano@gmail.com", "(83) 98900-0000", "PESSOA_FISICA");
 		controllerUsuarios.cadastraItem("1001", "cadeira de rodas", 5, "roda grande,cadeira");
 		controllerUsuarios.cadastraItem("1002", "colchao", 10, "colchao kingsize,conforto,dormir");
 		controllerUsuarios.cadastraItem("1234", "calca jeans", 2, "azul");
+		controllerUsuarios.cadastraItem("9090", "travesseiro", 10, "travesseiro de pena");
+		controllerUsuarios.cadastraItem("1001", "Livro", 10, "Infantil,Matematica,Didatico");
+		controllerUsuarios.lerReceptores("arquivos_sistema/novosReceptores.csv");
+		controllerUsuarios.cadastraItem("84473712044", "Livro", 1, "Infantil,Matematica,Didatico");
+		
 	}
 	
 	@DisplayName("Testa adicionaDoador null")
@@ -193,7 +199,6 @@ class ControllerUsuariosTest {
 	void testCadastraItemNull() {
 		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.cadastraItem("1001", null, 2, "all-in-one"));	
 		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.cadastraItem(null, "computador", 2, "all-in-one"));	
-		
 	}
 	
 	@DisplayName("Testa cadastraItem vazio")
@@ -215,11 +220,10 @@ class ControllerUsuariosTest {
 		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.cadastraItem("5625", "computador", 7, "all-in-one"));	
 	}
 	
-	
 	@DisplayName("Testa cadastraItem ok")
 	@Test
 	void testCadastraItemOk() {
-		assertEquals(controllerUsuarios.cadastraItem("1234", "computador", 7, "all-in-one"), 4);	
+		assertEquals(controllerUsuarios.cadastraItem("1234", "computador", 7, "all-in-one"), 7);	
 	}
 	
 	
@@ -235,7 +239,6 @@ class ControllerUsuariosTest {
 	@Test
 	void testExibeItemNull() {
 		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.exibeItem(1, null));		
-		
 	}
 	
 	@DisplayName("Testa exibeItem vazio")
@@ -244,14 +247,11 @@ class ControllerUsuariosTest {
 		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.exibeItem(1, ""));	
 	}
 	
-	
-	
 	@DisplayName("Testa exibeItem doador inexistente")
 	@Test
 	void testExibeItemInexistente() {
 		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.exibeItem(4, "12543"));	
 	}
-	
 	
 	@DisplayName("Testa exibeItem ok")
 	@Test
@@ -263,6 +263,159 @@ class ControllerUsuariosTest {
 	
 	
 	
+	@DisplayName("Testa atualizaItem quantidade menor que zero")
+	@Test
+	void testAtualizaItemMenorQueZero() {
+		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.atualizaItem(-1, "1234", 4, "azul, listrada"));	
+	}
+	
+	@DisplayName("Testa atualizaItem null")
+	@Test
+	void testAtualizaItemNull() {
+		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.atualizaItem(3, null, 3, "azul, listrada"));		
+		
+	}
+	
+	@DisplayName("Testa atualizaItem vazio")
+	@Test
+	void testAtualizaItemVazio() {
+		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.atualizaItem(3, "", 3, "azul, listrada"));	
+	}
+	
+	@DisplayName("Testa atualizaItem doador inexistente")
+	@Test
+	void testAtualizaItemInexistente() {
+		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.atualizaItem(3, "25632", 3, "azul, listrada"));	
+	}
+	
+	@DisplayName("Testa atualizaItem ok")
+	@Test
+	void testAtualizaItemOk() {
+		assertEquals(controllerUsuarios.atualizaItem(3, "1234", 5, "azul, listrada"), "3 - calca jeans, tags: [azul, listrada], quantidade: 5");	
+	}
+	
+	
+	
+	
+	
+	@DisplayName("Testa removeItem quantidade menor que zero")
+	@Test
+	void testRemoveItemMenorQueZero() {
+		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.removeItem(-1, "1234"));	
+	}
+	
+	@DisplayName("Testa removeItem null")
+	@Test
+	void testRemoveItemNull() {
+		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.removeItem(3, null));		
+	}
+	
+	@DisplayName("Testa removeItem vazio")
+	@Test
+	void testRemoveItemVazio() {
+		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.removeItem(3, ""));	
+	}
+	
+	@DisplayName("Testa removeItem doador inexistente")
+	@Test
+	void testRemoveItemInexistente() {
+		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.removeItem(3, "526541"));	
+	}
+	
+	@DisplayName("Testa removeItem ok")
+	@Test
+	void testRemoveItemOk() {
+		controllerUsuarios.removeItem(3, "1234");
+		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.removeItem(3, "1234"));	
+	}
+	
+	
+	
+	
+	
+	
+	@DisplayName("Testa pesquisaItemParaDoacaoPorDescricao null")
+	@Test
+	void testPesquisaItemParaDoacaoPorDescricaoNull() {
+		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.pesquisaItemParaDoacaoPorDescricao(null));	
+	}
+	
+	@DisplayName("Testa pesquisaItemParaDoacaoPorDescricao vazio")
+	@Test
+	void testPesquisaItemParaDoacaoPorDescricaoVazio() {
+		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.pesquisaItemParaDoacaoPorDescricao(""));
+	}
+	
+	@DisplayName("Testa pesquisaItemParaDoacaoPorDescricao ok")
+	@Test
+	void testPesquisaItemParaDoacaoPorDescricaoOk() {
+		assertEquals(controllerUsuarios.pesquisaItemParaDoacaoPorDescricao("travesseiro"), "4 - travesseiro, tags: [travesseiro de pena], quantidade: 10");
+	}
+	
+	
+	
+	@DisplayName("Testa match null")
+	@Test
+	void testMatchNull() {
+		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.match(null, 2));	
+	}
+	
+	@DisplayName("Testa match vazio")
+	@Test
+	void testMatchVazio() {
+		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.match("", 2));	
+	}
+	
+	@DisplayName("Testa match usuario nao receptor")
+	@Test
+	void testMatchUsuarioNaoReceptor() {
+		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.match("1234", 3));	
+	}
+	
+	@DisplayName("Testa match id negativo")
+	@Test
+	void testMatchIdNegativo() {
+		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.match("84473712044", -1));	
+	}
+	
+	@DisplayName("Testa match id ok")
+	@Test
+	void testMatchIdOk() {
+		assertEquals(controllerUsuarios.match("84473712044", 3), "3 - calca jeans, tags: [azul], quantidade: 2, doador: Manel/1234");	
+	}
+	
+	@DisplayName("Testa match item inexistente")
+	@Test
+	void testMatchIdItemInexistente() {
+		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.match("84473712044", 85));
+	}
+	
+	
+	
+	
+	@DisplayName("Testa realizaDoacao null")
+	@Test
+	void testRealizaDoacaoNull() {
+		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.realizaDoacao(3, 3, null));
+	}
+	
+	@DisplayName("Testa realizaDoacao vazio")
+	@Test
+	void testRealizaDoacaoVazio() {
+		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.realizaDoacao(3, 3, ""));
+	}
+	
+	@DisplayName("Testa realizaDoacao descricao diferente")
+	@Test
+	void testRealizaDoacaoDescricaoDiferente() {
+		assertThrows(IllegalArgumentException.class, () -> controllerUsuarios.realizaDoacao(3, 4, "13/12/2018"));
+	}
+	
+	@DisplayName("Testa realizaDoacao descricao ok")
+	@Test
+	void testRealizaDoacaoOk() {
+		assertEquals(controllerUsuarios.realizaDoacao(5, 6, "13/12/2018"), "13/12/2018 - doador: Murilo Luiz Brito/84473712044, item: livro, quantidade: 1, receptor: Elizabeth Ashe/1001");
+	}
 	
 	
 }
